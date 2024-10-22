386408c84175885889a5167e2a876429ac3b203c
mansibansal9648 <mansibansal12345@gmail.com>
2024-10-22 23:47:41 +0530

    basics of design patterns

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Design Patterns");
+
+        //Design Pattern
+        // Design patterns are the well proved solution of commonly occurring problems in software design
+        //Design patterns represent an idea
+
+
+        // Categorization of design patterns:-
+
+        // 1. Creational design patterns - related to creation of an object
+        // eg:- singleton pattern,factory pattern, builder pattern etc.
+
+        //2.Structural design patterns - related to structure of code
+        // eg:- proxy pattern, adaptor pattern etc.
+
+        //3.Behavioral design pattern - focus on interaction of objects (make application loosely couple)
+        // eg:- observer pattern,state pattern, iterator pattern etc.
     }
 }
